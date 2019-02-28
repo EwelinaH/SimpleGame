4336b4fec80329a8ebd7cf27e1b35339ea175508
EwelinaH <ewelina.hol2@gmail.com>
2019-02-28 23:00:39 +0100

    simple game tests

--- src/main/java/com/simplegame/tddjava/game/Game.java
+++ src/main/java/com/simplegame/tddjava/game/Game.java
@@ -1,4 +1,49 @@
 package com.simplegame.tddjava.game;
 
 public class Game {
+
+    private Character[] board = {'k', 'p', 'n'};
+
+    public String play(char player1, char player2){
+        isEmpty(player1, player2);
+        if(isWinP1(player1, player2)){
+            return "Wygral P1";
+        } else if (isWinP2(player1, player2)){
+            return "Wygral P2";
+        } else if (isDraw(player1, player2)){
+            return "remis";
+        }
+        return "Brak zwyciezcy";
+
+    }
+
+    private boolean isWinP1(char player1, char player2){
+        if (player1 == 'k' && player2 =='n' || player1 == 'n' && player2 =='p' || player1 == 'p' && player2 =='k'){
+            return true;
+
+        }
+        return false;
+    }
+
+    private boolean isWinP2(char player1, char player2){
+        if (player1 == 'n' && player2 =='k' || player1 == 'p' && player2 == 'n' || player1 == 'k' && player2 == 'p'){
+            return true;
+
+        }
+        return false;
+    }
+
+    private void isEmpty(char player1, char player2) {
+        if (player1 == '\0' || player2 == '\0') {
+            throw
+                    new RuntimeException("Trzeba wybrac jedna opcje");
+        }
+    }
+
+    private boolean isDraw(char player1, char player2){
+        if (player1 == player2){
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/com/simplegame/tddjava/game/GameSpec.java
+++ src/test/java/com/simplegame/tddjava/game/GameSpec.java
@@ -1,4 +1,69 @@
 package com.simplegame.tddjava.game;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+
 public class GameSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private Game game;
+
+    @Before
+    public final void before(){
+        game = new Game();
+    }
+
+    @Test
+    public void whenNothingWasSelected(){
+        exception.expect(RuntimeException.class);
+        game.play('\0', '\0');
+    }
+
+    @Test
+    public void whenSameSelectedThenNoWinner(){
+        String actual = game.play('k','k');
+        assertEquals("remis", actual);
+    }
+
+    @Test
+    public void whenP1hasKAndP2hasNThenLose(){
+        String actual = game.play('k','n');
+        assertEquals("Wygral P1", actual);
+    }
+
+    @Test
+    public void whenP1hasNAndP2hasPThenLose(){
+        String actual = game.play('n','p');
+        assertEquals("Wygral P1", actual);
+    }
+
+    @Test
+    public void whenP1hasPAndP2hasKThenLose(){
+        String actual = game.play('p','k');
+        assertEquals("Wygral P1", actual);
+    }
+
+    @Test
+    public void whenP1hasNAndP2hasKThenLose(){
+        String actual = game.play('n','k');
+        assertEquals("Wygral P2", actual);
+    }
+
+    @Test
+    public void whenP1hasPAndP2hasNThenLose(){
+        String actual = game.play('p','n');
+        assertEquals("Wygral P2", actual);
+    }
+
+    @Test
+    public void whenP1hasKAndP2hasPThenLose(){
+        String actual = game.play('k','p');
+        assertEquals("Wygral P2", actual);
+    }
+
 }
